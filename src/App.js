2b428b62fcc9caca55885dd25dc0c5c91ef350a0
Ke Quang <trankequang9221@gmail.com>
2023-04-09 10:46:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import QRCode from 'qrcode.react';
+import { useState, useEffect } from 'react';
 
 function App() {
+  let eventSource = undefined;
+
+  useEffect(() => {
+    eventSource = registerSSE("https://tdshop.herokuapp.com/api/v1/order/register-client");
+    return () => {
+      eventSource.close();
+      console.log("event closed")
+    }
+
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <div>
+          <QRCode
+            id='qrcode'
+            value='momo://app?action=payWithApp&isScanQR=true&serviceType=qr&sid=TU9NT3xhYmQzODk5Ni1hMTQ0LTRiYTEtYTI3MS00Mjc5YTg2NTZhYTQ&v=2.3'
+            size={290}
+            level={'H'}
+            includeMargin={true}
+          />
+        </div>
       </header>
     </div>
   );
+}
+
+function registerSSE(url) {
+  const source = new EventSource(url);
+  source.addEventListener('payment_result', event => {
+      handlePaymentResultEvent(JSON.parse(event.data));
+  })
+  source.onopen = event => console.log("Connection opened");
+  source.onerror = event => console.error("Connection error");
+  return source;
+}
+
+function handlePaymentResultEvent(data) {
+  console.log(data);
 }
 
 export default App;
